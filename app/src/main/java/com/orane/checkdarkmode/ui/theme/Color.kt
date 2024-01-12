acccbb53b81231365b8e9443c8777aa2dcda2b98
kannan-krishnan <kannanramakrishnan@icliniq.com>
2024-01-12 15:47:21 +0530

    Initial commit

---
+++ app/src/main/java/com/orane/checkdarkmode/ui/theme/Color.kt
@@ -0,0 +1,26 @@
+package com.orane.checkdarkmode.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+
+//light
+val PrimaryLight = Color(0xFF1B72C0)
+val SurfaceLight = Color(0xFFF3F4F9)
+val PrimaryContainerLight = Color(0xFFD3E4FF)
+val OnPrimaryContainerLight = Color(0xFF001C38)
+val OnSecondarySurfaceLight = Color(0xFFEFF1F8)
+val backgroundLight = Color(0xFFFCFCFF)
+val OnSurface20Light = Color(0xFF001E2F)
+val OnSurface40Light = Color(0xFF44474E)
+val OnSurface60Light = Color(0xFF74777F)
+
+//dark
+val PrimaryDark = Color(0xFFA2C9FF)
+val SurfaceDark = Color(0xFF13232C)
+val PrimaryContainerDark = Color(0xFF2F4156)
+val OnPrimaryContainerDark = Color(0xFFD7E2FF)
+val OnSecondarySurfaceDark = Color(0xFF1E2A32)
+val backgroundDark = Color(0xFF0E181E)
+val OnSurface20Dark = Color(0xFFE0F1FF)
+val OnSurface40Dark = Color(0xFFBFC6DA)
+val OnSurface60Dark = Color(0xFFA8ADBD)
